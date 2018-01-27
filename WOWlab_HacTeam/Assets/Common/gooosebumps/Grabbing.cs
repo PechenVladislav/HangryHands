2c9e5c5316f6188789d6f4cd56e3580c2786a16b
Gooosebumps <gooosebumps@mail.ru>
2018-01-27 15:52:52 +0300

    grabbing object

---
+++ WOWlab_HacTeam/Assets/Common/gooosebumps/Grabbing.cs
@@ -0,0 +1,17 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class Grabbing : MonoBehaviour
+{
+
+    public void OnTriggerStay(Collider other)
+    {
+        if (SteamVR_Controller.Input(SteamVR_Controller.GetDeviceIndex(SteamVR_Controller.DeviceRelation.Leftmost)).GetHairTriggerDown())
+        {
+            transform.parent = other.transform.parent;
+        }
+    }
+}
