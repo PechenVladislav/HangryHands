2722638d983465d4016b0bf5541528c277106d89
Vlad Pechen <pechenvladislav@gmail.com>
2018-01-27 22:12:29 +0300

    Hands rotation fix

---
+++ WOWlab_HacTeam/Assets/Scripts/HandController.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandController : MonoBehaviour
+{
+
+    [SerializeField]
+    Transform ControllerTransform;
+    [SerializeField]
+    HandLogic HandLogic;
+    [SerializeField]
+    Rigidbody Rigidbody;
+    [SerializeField]
+    float Speed;
+    [SerializeField]
+    Transform snapFoodPoint;
+    [SerializeField]
+    Transform handPositionOnController;
+
+    public bool MoveBack { get; private set; }
+
+    // Use this for initialization
+    void Start()
+    {
+        throwHand = false;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (throwHand)
+        {
+            Rigidbody.velocity = ControllerTransform.up * Speed;
+        }
+        else if (MoveBack)
+        {
+            StartCoroutine(MoveBackCoroutine());
+        }
+    }
+    bool throwHand;
+
+
+    IEnumerator MoveBackCoroutine()
+    {
+        Vector3 startPos = transform.position;
+        float dist = (transform.position - ControllerTransform.position).magnitude;
+        float speed = 1;
+        float t = 0f;
+        while (t <= 1f)
+        {
+            t += Time.deltaTime / dist * speed;
+            transform.position = Vector3.Lerp(startPos, ControllerTransform.position, t);
+            yield return null;
+        }
+
+        //transform.position
+    }
+
+
+    public void Thorw()
+    {
+        throwHand = true;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        //if (other.GetComponent<Grabbing>())//&& HandLogic.Squeeze
+        //{
+        //    Grabbing food = other.GetComponent<Grabbing>().Catch(snapPoint);
+        //    MoveBack = true;
+        //    throwHand = false;
+        //    yield return null;
+        //}
+    }
+
+
+    private void Return()
+    {
+        MoveBack = true;
+    }
+}
