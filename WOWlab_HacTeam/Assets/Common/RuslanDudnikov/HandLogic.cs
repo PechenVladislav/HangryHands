a2e2aad456f23601c45556575a5e1463ad5248b9
Ruslan Dudnikov <jerryletehen@gmail.com>
2018-01-27 11:09:09 +0300

    HandLogic: first step + GameManager

--- WOWlab_HacTeam/Assets/Common/RuslanDudnikov/HandLogic.cs
+++ WOWlab_HacTeam/Assets/Common/RuslanDudnikov/HandLogic.cs
@@ -3,17 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+public enum HandStates { NotREadyToShoot, ReadyToShoot }
 
 public class HandLogic : MonoBehaviour {
 
 
+
+
 	[SerializeField] GameObject leftHand;
 	[SerializeField] GameObject rightHand;
+    private SteamVR_Controller.Device controller;
 
 	[SerializeField] Text testText1;
 	[SerializeField] Text testText2;
 	[SerializeField] Text testText3;
 	[SerializeField] Text testText4;
+
+    private GameObject sphere;
+    
 
 	private Vector3 posLeftHand;
 	private Vector3 posRightHand;
@@ -23,50 +30,94 @@ public class HandLogic : MonoBehaviour {
 
 	private float biggerSpeed = 0f;
 
+    private bool IsPressed;
+
+    public float CurrentSpeed { get; private set; }
+
+    public HandStates handState;
+
 	private void Awake()
 	{
 		posLeftHand = leftHand.transform.position;
 		posRightHand = rightHand.transform.position;
 		lastPosLeftHand = Vector3.zero;
+        handState = HandStates.NotREadyToShoot;
+        sphere = GameObject.Find("OurSphere");
+        sphere.SetActive(false);
 	}
 
 	private void FixedUpdate()
 	{
+        IsPressed = controller.GetPressDown(Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger);
 
+        if(IsPressed && CurrentSpeed >= GameManager.Instance.minSpeed)
+        {
+            StartCoroutine(WaitTimeToShoot());
+        } else {
+            StopCoroutine(WaitTimeToShoot());
+            handState = HandStates.NotREadyToShoot;
+        }
 
-
-			
+        if(handState == HandStates.ReadyToShoot && !IsPressed)
+        {
+            Shoot();
+        }
 	}
 
+    private void Shoot()
+    {
+        sphere.SetActive(true);
+    }
 
-	private void Tests()
+    private IEnumerator WaitTimeToShoot()
+    {
+        var startTime = Time.realtimeSinceStartup;
+        var endTime = startTime + GameManager.Instance.minTimeShoot;
+        while(IsPressed && CurrentSpeed >= GameManager.Instance.minSpeed && Time.realtimeSinceStartup < endTime)
+        {
+            yield return new WaitForFixedUpdate();
+        }
+        handState = HandStates.ReadyToShoot;
+    }
+
+
+    private void Update()
+    {
+        Tests();
+    }
+
+
+
+    private void Tests()
 	{
+
+        // text 1
 		posLeftHand = leftHand.transform.position;
 		posRightHand = rightHand.transform.position;
 		var distance = Vector3.Distance (posLeftHand, posRightHand);
 		testText1.text = "Distance btwn hands: " + distance;
 
+        //text 2
+        testText2.text = "Trigger : " + IsPressed;
 
+
+        // text 3
 		float currentDistance = Vector3.Distance (leftHand.transform.position, lastPosLeftHand);
-		if (currentDistance > biggerDistance) {
-			biggerDistance = currentDistance;
-		}
-		testText2.text = "bigger : " + biggerDistance;
 
-		var currentSpeed = currentDistance / Time.deltaTime;
-		testText3.text = "current speed : " + currentSpeed;
+		CurrentSpeed = currentDistance / Time.deltaTime;
+		testText3.text = "current speed : " + CurrentSpeed;
 
 		lastPosLeftHand = posLeftHand;
 
-
-
-		if (currentSpeed > biggerSpeed) {
-			biggerSpeed = currentSpeed;
+		if (CurrentSpeed > biggerSpeed) {
+			biggerSpeed = CurrentSpeed;
 			if (biggerSpeed > 100) {
 				biggerSpeed = 0f;
 			}
 		}
 
+
+        // text 4
 		testText4.text = "bigger speed : " + biggerSpeed;
 	}
 
