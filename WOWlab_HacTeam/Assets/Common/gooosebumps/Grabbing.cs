2d2edb6944b35aa572736b32f0705c0010f17e19
Gooosebumps <gooosebumps@mail.ru>
2018-01-27 21:50:21 +0300

    new scripts added

--- WOWlab_HacTeam/Assets/Common/gooosebumps/Grabbing.cs
+++ WOWlab_HacTeam/Assets/Common/gooosebumps/Grabbing.cs
@@ -2,16 +2,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Valve.VR;
+
 
 public class Grabbing : MonoBehaviour
 {
 
-    public void OnTriggerStay(Collider other)
+    public GameObject Banana;
+
+    public void Catch(Transform newParent)
+    //public void OnTriggerStay(Collider other)
     {
-        if (SteamVR_Controller.Input(SteamVR_Controller.GetDeviceIndex(SteamVR_Controller.DeviceRelation.Leftmost)).GetHairTriggerDown())
-        {
-            transform.parent = other.transform.parent;
-        }
+        Banana.transform.SetParent(newParent);
+        transform.localPosition = Vector3.zero;
+        transform.localRotation = Quaternion.identity;
     }
 }
+
+
+// if (SteamVR_Controller.Input(SteamVR_Controller.GetDeviceIndex(SteamVR_Controller.DeviceRelation.Leftmost)).GetHairTriggerDown())
+// схватить по нажатию на кнопку как-то так
