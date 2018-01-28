07bf92e316408a3fd9945854406a2e02644bca31
Gooosebumps <gooosebumps@mail.ru>
2018-01-28 14:01:52 +0300

    spawning trail added

---
+++ WOWlab_HacTeam/Assets/Common/gooosebumps/movePlayer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movePlayer : MonoBehaviour {
+
+    public static int movespeed = 5;
+    public GameObject movingObj;
+    public Vector3 userDirection = Vector3.right;
+    //public List<GameObject> trail = new List<GameObject>();
+    //public GameObject trailObject = null;
+    //public Transform currentPosition;
+
+
+
+
+    public void Start()
+    {
+        //trailObject = Instantiate(Resources.Load("Prefabs/Cube")) as GameObject;
+        // currentPosition = transform.
+    }
+
+    public void Update()
+    {
+        transform.Translate(userDirection * movespeed * Time.deltaTime);
+
+
+        //GameObject t = new GameObject("trailObject");
+        //trail.Add(Instantiate(t));
+        // Instantiate(trailObject);
+
+    }
+}
---
+++ WOWlab_HacTeam/Assets/Common/gooosebumps/trail.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trail : MonoBehaviour {
+
+    public GameObject hand;
+    public Transform spawnPoints;
+    public float spawnTime;
+
+
+    void Start () {
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+
+        // Spawn();
+    }
+	
+	void Update () {
+        
+    }
+
+    void Spawn()
+    {
+        Instantiate(hand, spawnPoints.position, spawnPoints.rotation);
+    }
+}
